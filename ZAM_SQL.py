c7c0c6d6ab763bd7ef2fb71cbcde98bf3b57e0e9
Zyad98Mackawy <42006557+Zyad98Mackawy@users.noreply.github.com>
2021-01-10 18:42:08 +0200

    Add files via upload

---
+++ ZAM_SQL.py
@@ -0,0 +1,32 @@
+DT_sql = ['INTEGER','TEXT','REAL','BLOB','NULL']
+
+def DB_CT(table_name = "zyad",columns=['fname'],type_data=[DT_sql[1]]):
+    instruction = "CREATE TABLE " + table_name + " ( "
+    #instruction =     """CREATE TABLE z(fname text,zipcode integer)"""
+    for i in range(len(columns)):
+        if i != 0 :
+            instruction = instruction +" , "
+        instruction = instruction + columns[i] + " " + type_data[i]
+    instruction = instruction + " );"
+    print(instruction)
+    return instruction
+
+def DB_insert(table_name='zyad',columns=['fname'],values=['zyaaaad'],type_data=[DT_sql[1]]):
+    instruction = "INSERT INTO " + table_name + " ("
+    part_VALUES="VALUES ("
+    for i in range(len(values)):
+        if i != 0 :
+            instruction = instruction +" , "
+            part_VALUES = part_VALUES +" , "
+        if type_data[i] == DT_sql[1] :
+            part_VALUES = part_VALUES +"'"+values[i]+"'"
+        else:
+            part_VALUES = part_VALUES + values[i]
+        instruction = instruction + columns[i]
+    instruction = instruction + ") " + part_VALUES + ");"
+    print(instruction)
+    return instruction
+
+
+if __name__ == '__main__' :
+    print(' this is made by zyad mackawy ')
